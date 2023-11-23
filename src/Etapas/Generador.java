6b7adddc4dcbceb9fcf8dce461aceb5bb488fa61
Valentin Arregui <varregui@alumnos.exa.unicen.edu.ar>
2023-11-22 22:18:00 -0300

    while2

--- src/Etapas/Generador.java
+++ src/Etapas/Generador.java
@@ -87,8 +87,7 @@ public abstract class Generador {
         mapa.put("<=", new EstructuraComparador("JA"));
         mapa.put("=!", new EstructuraComparador("JE"));
         mapa.put("Asignacion", new EstructuraAsignacion());
-        mapa.put("while", nodo -> {var e = new EstructuraWhile();
-            return "etiqueta" + etiqueta + ":\n" + GenerarCodigo(nodo.getIzq()) + e.generar(nodo);});  //todo: revisar funcionamiento, creo esta mal
+        mapa.put("while", new EstructuraWhile()); 
         mapa.put("sentencias", noAction);
         mapa.put("cuerpoIf", noAction);
         mapa.put("if", noAction);
